ad2776bda7dff142f16f5b22815c57b67f344c1e
João Rodolfo <jr_costa16@hotmail.com>
2024-04-03 17:03:10 -0300

    Atividades dia 03/04

--- Atividade/js/script.js
+++ Atividade/js/script.js
@@ -113,32 +113,48 @@ const exercicio7 = () => {
 };
 
 const exercicio8 = () => {
+  //   let palavra = document.getElementById("palavra").value;
+  //   let error = document.getElementById("erro");
+  //   let result = document.getElementById("resultado");
+
+  //   error = "";
+  //   result = "";
+
+  //   if (palavra === "") {
+  //     error = "Digite uma palavra válida!";
+  //   } else {
+  //     let palavraInvertida = palavra.length - 1;
+  //     let ePalindromo = true;
+  //     for (let i = 1; i < palavraInvertida; i++, palavraInvertida--) {
+  //       if (palavra[i] !== palavra[palavraInvertida]) {
+  //         ePalindromo = false;
+  //         break;
+  //       }
+  //     }
+  //     if (ePalindromo) {
+  //       result = `"${palavra}" é um palíndromo!`;
+  //     } else {
+  //       result = `"${palavra}" não é um palíndromo.`;
+  //     }
+  //   }
+  //   erro.innerHTML = error;
+  //   resultado.innerHTML = result;
+
   let palavra = document.getElementById("palavra").value;
-  let erro = document.getElementById("erro");
-  let resultado = document.getElementById("resultado");
+  let error = document.getElementById("erro");
+  let result = document.getElementById("resultado");
+  error.innerText = "";
+  result.innerText = "";
 
-  let error = "";
-  let result = "";
+  let inversa = "";
 
-  if (palavra === "") {
-    error = "Digite uma palavra válida!";
-  } else {
-    let palavraInvertida = palavra.length - 1;
-    let ePalindromo = true;
-    for (let i = 0; i < palavraInvertida; i++, palavraInvertida--) {
-      if (palavra[i] !== palavra[palavraInvertida]) {
-        ePalindromo = false;
-        break;
-      }
-    }
-    if (ePalindromo) {
-      result = `"${palavra}" é um palíndromo!`;
-    } else {
-      result = `"${palavra}" não é um palíndromo.`;
-    }
+  for (let i = 1; i <= palavra.length; i++) {
+    inversa += palavra.charAt(palavra.length - i);
   }
-  erro.innerHTML = error;
-  resultado.innerHTML = result;
+
+  palavra.toLowerCase() == inversa.toLowerCase()
+    ? (result.innerText = "A palavra é um palíndromo!")
+    : (error.innerText = "A palavra não é um palíndromo!");
 };
 
 const exercicio9 = () => {
@@ -183,3 +199,193 @@ const exercicio11 = () => {
     contador--;
   }
 };
+
+const exercicio12 = () => {
+  let num = document.getElementById("num").value;
+  let result = document.getElementById("resultado");
+  let error = document.getElementById("erro");
+  let soma = 0;
+
+  result.innerText = "";
+
+  if (num <= 9) {
+    error.innerHTML = `Informe um número válido`;
+  } else {
+    for (let i = 1; i <= num.length; i++) {
+      soma += parseInt(num.charAt(num.length - i));
+    }
+    result.innerHTML = soma;
+  }
+};
+
+const exercicio13 = () => {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let error = document.getElementById("erro");
+  let result = document.getElementById("resultado");
+
+  result.innerText = "";
+  error.innerText = "";
+  if (num1 >= num2 || num1 < 1) {
+    error.innerHTML = `Informe números válidos`;
+  } else {
+    for (let i = num1; i <= num2; i++) {
+      let teste = verificarPrimo(i);
+      if (teste) {
+        result.innerText += ` ${i}`;
+      }
+    }
+  }
+};
+
+const verificarPrimo = (num) => {
+  let primo = true;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) primo = false;
+  }
+
+  return primo;
+};
+
+const exercicio14 = () => {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  if (num1 <= 0 || num2 <= 0) {
+    document.getElementById("erro").innerText = "Digite um valor maior que 0";
+    document.getElementById("resultado").innerText = ``;
+  } else {
+    document.getElementById("resultado").innerText = `A área do retângulo é ${
+      num1 * num2
+    }`;
+    document.getElementById("erro").innerText = "";
+  }
+};
+
+const exercicio15 = () => {
+  let palavra = document.getElementById("palavra").value;
+  let result = document.getElementById("resultado");
+  let error = document.getElementById("erro");
+
+  let vogais = "";
+  let consoantes = "";
+  let nrLetras = palavra.length;
+
+  palavra = palavra.toLowerCase();
+  for (let i = 0; i < nrLetras; i++) {
+    if (
+      palavra[i] == "a" ||
+      palavra[i] == "e" ||
+      palavra[i] == "i" ||
+      palavra[i] == "o" ||
+      palavra[i] == "u"
+    ) {
+      vogais += ` ${palavra[i]}`;
+    } else {
+      consoantes += ` ${palavra[i]}`;
+    }
+  }
+  result.innerText = `Vogais: ${vogais} - Consoantes: ${consoantes} `;
+};
+
+const exercicio16 = () => {
+  let num1 = document.getElementById("num1").value;
+  if (num1 <= 0) {
+    document.getElementById("erro").innerText = "Digite um valor maior que 0";
+    document.getElementById("resultado").innerText = ``;
+  } else {
+    document.getElementById("resultado").innerText = `A área do círculo é ${
+      3.14 * num1 ** 2
+    }`;
+    document.getElementById("erro").innerText = "";
+  }
+};
+
+const exercicio17 = () => {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  if (num1 <= 0 || num2 <= 0) {
+    document.getElementById("erro").innerText = "Digite um valor maior que 0";
+    document.getElementById("resultado").innerText = ``;
+  } else {
+    document.getElementById("resultado").innerText = `A área do retângulo é ${
+      (num1 * num2) / 2
+    }`;
+    document.getElementById("erro").innerText = "";
+  }
+};
+
+const exercicio18 = () => {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let num3 = document.getElementById("num3").value;
+  if (num1 <= 0 || num2 <= 0 || num3 <= 0) {
+    document.getElementById("erro").innerText = "Digite um valor maior que 0";
+    document.getElementById("resultado").innerText = ``;
+  } else {
+    document.getElementById("resultado").innerText = `A área do trapézio é ${
+      ((parseInt(num1) + parseInt(num2)) * parseInt(num3)) / 2
+    }`;
+    document.getElementById("erro").innerText = "";
+  }
+};
+
+const exercicio19 = () => {
+  let data = document.getElementById("data").value;
+  let error = document.getElementById("erro");
+  let result = document.getElementById("resultado");
+  let dataNasc = new Date(data);
+  let dataAtual = new Date();
+  let diferenca = dataAtual.getTime() - dataNasc.getTime();
+  const idade = Math.floor(diferenca / (1000 * 60 * 60 * 24 * 365.25));
+
+  if (data == "") {
+    error.innerText = `Digite uma data válida`;
+  } else {
+    error.innerText = "";
+    result.innerText = `A idade é de ${idade}`;
+  }
+};
+
+const inverterFrase = (frase) => {
+  const palavras = frase.split(" ");
+  return palavras.reverse().join(" ");
+};
+
+const exercicio20 = () => {
+  let frase = document.getElementById("frase").value;
+  let result = document.getElementById("resultado");
+  let error = document.getElementById("erro");
+
+  result.innerText = "";
+  error.innerText = "";
+  let palavras = frase.split(" ");
+
+  if (frase == "") {
+    error.innerText = `Digite uma frase válida`;
+  } else {
+    result.innerHTML = `${palavras.reverse().join(" ")}`;
+  }
+};
+
+const exercicio21 = () => {
+  let frase = document.getElementById("frase").value;
+  let result = document.getElementById("resultado");
+  let error = document.getElementById("erro");
+
+  result.innerText = "";
+  error.innerText = "";
+  let novoTexto = frase.split(" ");
+
+  if (frase == "") {
+    error.innerHTML = `Informe uma frase válida`;
+  } else {
+    result.innerHTML = `${novoTexto.join("")}`;
+  }
+};
+
+const exercicio22 = () => {};
+
+const exercicio23 = () => {};
+
+const exercicio24 = () => {};
